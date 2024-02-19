ccf990f1f4a9edb11ce7886fa5cc21fa9d9569de
Putoshi <silicopro@gmail.com>
2024-02-19 23:45:50 +0900

    tfの物体検知にバウンディングボックスを表示

--- src/renderer/components/HumanDetection.tsx
+++ src/renderer/components/HumanDetection.tsx
@@ -1,7 +1,7 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useFaceDetection } from 'react-use-face-detection';
 import FaceDetection from '@mediapipe/face_detection';
-import baseImg from '../../../assets/test4.jpg';
+import baseImg from '../../../assets/test5.jpg';
 
 require('@tensorflow/tfjs-backend-cpu');
 require('@tensorflow/tfjs-backend-webgl');
@@ -9,47 +9,49 @@ const cocoSsd = require('@tensorflow-models/coco-ssd');
 
 function HumanDetection() {
   const imgRef = useRef(null);
+  const [boundingBox, setBoundingBox] = useState([]);
   useEffect(() => {
     const img = imgRef.current;
     if (img) {
-      const model = cocoSsd.load();
-      model.then((model) => {
-        model.detect(img).then((predictions) => {
+      cocoSsd
+        .load()
+        .then((model: any) => {
+          return model.detect(img);
+        })
+        .then((predictions: any) => {
           console.log('Predictions: ');
           console.log(predictions);
+          setBoundingBox(predictions);
+          return predictions;
+        })
+        .catch((error: any) => {
+          console.error('Error:', error);
         });
-      });
     }
   }, [imgRef]);
-  // tf.setBackend('cpu').then(() => main());
 
-  // // Load the model.
-  // const model = await cocoSsd.load();
-
-  // // Classify the image.
-  // const predictions = await model.detect(img);
-
-  // console.log('Predictions: ');
-  // console.log(predictions);
-
-  // const getAssetPath = (...paths: string[]): string => {
-  //   return path.join(RESOURCES_PATH, ...paths);
-  // };
-  // const { imgRef, boundingBox, isLoading, detected, facesDetected } =
-  //   useFaceDetection({
-  //     faceDetectionOptions: {
-  //       model: 'short',
-  //     },
-  //     handleOnResults: (results) => {
-  //       console.log(results);
-  //     },
-  //     faceDetection: new FaceDetection.FaceDetection({
-  //       locateFile: (file) =>
-  //         `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection@0.4.1646425229/${file}`,
-  //     }),
-  //   });
   return (
-    <div>
+    <div
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      {boundingBox.map((box, index) => (
+        <div
+          key={`${index + 1}`}
+          style={{
+            border: '4px solid red',
+            position: 'absolute',
+            top: `${box.bbox[1]}px`,
+            left: `${box.bbox[0]}px`,
+            width: `${box.bbox[2]}px`,
+            height: `${box.bbox[3]}px`,
+            zIndex: 1,
+          }}
+        />
+      ))}
       <img
         crossOrigin="anonymous"
         ref={imgRef}
@@ -67,50 +69,7 @@ function HumanDetection() {
           objectFit: 'fill',
         }}
         src={baseImg}
-        // src="https://previews.123rf.com/images/gdolgikh/gdolgikh1504/gdolgikh150400087/38738606-gruppe-gl%C3%BCckliche-junge-leute-isoliert-auf-wei%C3%9Fem-hintergrund.jpg"
       />
-
-      {/* <p>{`Loading: ${isLoading}`}</p>
-      <p>{`Face Detected: ${detected}`}</p>
-      <p>{`Number of faces detected: ${facesDetected}`}</p>
-      <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
-        {boundingBox.map((box, index) => (
-          <div
-            key={`${index + 1}`}
-            style={{
-              border: '4px solid red',
-              position: 'absolute',
-              top: `${box.yCenter * 100}%`,
-              left: `${box.xCenter * 100}%`,
-              width: `${box.width * 100}%`,
-              height: `${box.height * 100}%`,
-              zIndex: 1,
-            }}
-          />
-        ))}
-        <img
-          crossOrigin="anonymous"
-          ref={imgRef}
-          alt=""
-          style={{
-            position: 'absolute',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            left: '0',
-            right: '0',
-            textAlign: 'center',
-            zIndex: 0,
-            width: '100%',
-            height: '100%',
-            objectFit: 'fill',
-          }}
-          src={baseImg}
-          handleOnResults={(results) => {
-            console.log(results);
-          }}
-          // src="https://previews.123rf.com/images/gdolgikh/gdolgikh1504/gdolgikh150400087/38738606-gruppe-gl%C3%BCckliche-junge-leute-isoliert-auf-wei%C3%9Fem-hintergrund.jpg"
-        />
-      </div> */}
     </div>
   );
 }
