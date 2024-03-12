89ebc0bdb0ad577f1af85ca10eec4a2391d50851
Putoshi <silicopro@gmail.com>
2024-03-12 18:39:47 +0900

    Reactのimport文を修正しました

--- src/renderer/components/HumanDetection.tsx
+++ src/renderer/components/HumanDetection.tsx
@@ -1,5 +1,6 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import LeonardoAIOptions from '../../main/LeonardoAIOptions';
+
 require('@tensorflow/tfjs-backend-cpu');
 require('@tensorflow/tfjs-backend-webgl');
 const cocoSsd = require('@tensorflow-models/coco-ssd');
